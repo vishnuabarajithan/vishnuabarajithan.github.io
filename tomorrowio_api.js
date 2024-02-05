642e1a6f9672a65f2739d3ccf15fe2c9bceccc35
Vishnu Abarajithan <vishnuaparajit1992@gmail.com>
2024-02-05 17:04:24 +0530

    Update tomorrowio_api.js

--- tomorrowio_api.js
+++ tomorrowio_api.js
@@ -37,29 +37,38 @@
             url: "https://api.tomorrow.io/v4/timelines?location=42.3478%2C%20-71.0466&fields=cloudCeiling,dewPoint,pressureSurfaceLevel&units=metric&apikey=" + apiKey,
             type: "GET",
             dataType: "json",
-            success: function (resp) {
-                var timelines = resp.data.timelines[0].intervals,
-                    tableData = [];
+            success: function (resp, status, xhr) {
+                if (xhr.status === 200) {
+                    var timelines = resp.data.timelines[0].intervals,
+                        tableData = [];
 
-                // Check if timelines is defined and not empty
-                if (timelines && timelines.length > 0) {
-                    // Iterate over the intervals array
-                    for (const interval of timelines) {
-                        const rowData = {
-                            time: new Date(interval.startTime),
-                            cloudCeiling: interval.values.cloudCeiling,
-                            dewPoint: interval.values.dewPoint,
-                            pressureSurfaceLevel: interval.values.pressureSurfaceLevel
-                        };
-                        tableData.push(rowData);
+                    // Check if timelines is defined and not empty
+                    if (timelines && timelines.length > 0) {
+                        // Iterate over the intervals array
+                        for (const interval of timelines) {
+                            const rowData = {
+                                time: new Date(interval.startTime),
+                                cloudCeiling: interval.values.cloudCeiling,
+                                dewPoint: interval.values.dewPoint,
+                                pressureSurfaceLevel: interval.values.pressureSurfaceLevel
+                            };
+                            tableData.push(rowData);
+                        }
+                        table.appendRows(tableData);
+                    } else {
+                        console.error("Error: No data found in the response.");
                     }
-                }
 
-                table.appendRows(tableData);
-                doneCallback();
+                    doneCallback();
+                } else {
+                    console.error("Error fetching data. HTTP status code:", xhr.status);
+                    alert("Unable to fetch data. Please enter a valid API Key.");
+                    doneCallback();
+                }
             },
             error: function (xhr, status, error) {
                 console.error("Error fetching data:", error);
+                alert("Unable to fetch data. Please enter a valid API Key.");
                 doneCallback();
             }
         });
